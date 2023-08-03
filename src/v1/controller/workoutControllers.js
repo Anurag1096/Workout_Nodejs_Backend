1626492a860b8b4e6b40ea5c6c34ac216cd6b9c6
Anurag1096 <anuragqwerty09@outlook.com>
2023-08-03 13:29:57 +0530

    some commit

--- src/v1/controller/workoutControllers.js
+++ src/v1/controller/workoutControllers.js
@@ -24,7 +24,7 @@ const createNewWorkout=(req,res)=>{
         const createWorkout= WorkoutService.createNewWorkout(newWorkout);
         res.status(201).send({status:"ok created",message:"created workout succesfully",data:createWorkout});
     }catch(error){
-        res.status(200).send({status:"failed",error:"cannot create new workout"})
+        res.status(200).send({status:"failed",error:"cannot create new workout" })
     }
 }
 // patch
