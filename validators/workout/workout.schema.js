e3bed9ae16919b817bfcf3fd01173e94c4b4fd16
Anurag chakravarty <60540331+Anurag1096@users.noreply.github.com>
2024-11-26 16:40:10 +0530

    Update workout.schema.js

--- validators/workout/workout.schema.js
+++ validators/workout/workout.schema.js
@@ -7,7 +7,7 @@ let schema = {
        "name": yup.string().required(),
        "mode":yup.string().required() ,
        "equipment":yup.array().max(5,"Only 5 equipment are allowed").required("atleast one equipment should be provided"),
-       "exercises":yup.array().max(10,"Only 10 tags are allowed").required("atleast one tag should be provided"),
+       "exercises":yup.array().max(10,"Only 10 tags are allowed").required("atleast three tag should be provided"),
     })
 }
 module.exports=schema
