88a1ac578d77cc0b0a72eb199ee653d535518323
Anurag chakravarty <60540331+Anurag1096@users.noreply.github.com>
2024-12-19 19:28:52 +0530

    Update workout.schema.js

--- validators/workout/workout.schema.js
+++ validators/workout/workout.schema.js
@@ -5,6 +5,7 @@ const yup =require("yup");
 let schema = {
     workout:yup.object({
        "name": yup.string().required(),
+        "age": yup.number().required(),
        "mode":yup.string().required() ,
        "equipment":yup.array().max(5,"Only 5 equipment are allowed").required("atleast one equipment should be provided"),
        "exercises":yup.array().max(10,"Only 10 tags are allowed").required("atleast three tag should be provided"),
